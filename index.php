78ff1659530ad9a4577bcdef95c1f7fc8bf40e32
Robert Tabor <bobtabor@q.com>
2015-02-02 16:35:20 -0700

    first commit

---
+++ index.php
@@ -0,0 +1,68 @@
+<?php 
+
+class Validate {
+	
+}
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>m3phpproject</title>
+	<style type="text/css">
+		div {
+			padding: 20px;
+		}
+	</style>
+</head>
+<body>
+	<form action="">
+		<div>
+			<select name="select1" id="">
+				<option value="">email</option>
+				<option value="">username</option>
+				<option value="">password</option>
+				<option value="">phone</option>
+				<option value="">number</option>
+			</select>
+			<input type="text" name="input1">
+		</div>
+		<div>
+			<select name="select2" id="">
+				<option value="">email</option>
+				<option value="">username</option>
+				<option value="">password</option>
+				<option value="">phone</option>
+				<option value="">number</option>
+			</select>
+			<input type="text" name="select2">
+		</div>
+		<div>
+			<select name="select3" id="">
+				<option value="">email</option>
+				<option value="">username</option>
+				<option value="">password</option>
+				<option value="">phone</option>
+				<option value="">number</option>
+			</select>
+			<input type="text" name="input3">
+		</div>
+		<div>
+			<select name="select4" id="">
+				<option value="">email</option>
+				<option value="">username</option>
+				<option value="">password</option>
+				<option value="">phone</option>
+				<option value="">number</option>
+			</select>
+			<input type="text" name="input4">
+		</div>
+		<div>	
+			<button>Submit</button>
+		</div>
+	</form>
+</body>
+</html>
